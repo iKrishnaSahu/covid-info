7b531279944b960f44e54de312da61023986178e
Krishna Sahu <krishnajsahu@gmail.com>
2021-05-14 10:52:47 +0530

    Add today and tomorrow vaccine availability check

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,10 +12,12 @@ import { Vibration } from '@ionic-native/vibration/ngx';
 export class HomePage {
 
   pincode = '';
-  availableVaccineHospitalList: { name: string; numberOfVaccineAvailable: number }[] = [];
+  availableVaccineHospitalList: VaccineAvailability[] = [];
   interval;
   updatedOn;
-  currentDate;
+  today;
+  tommorrow;
+  isFetching = false;
 
   constructor(private dataService: DataService, private vibration: Vibration) { }
 
@@ -25,20 +27,40 @@ export class HomePage {
 
   getData() {
     if (!_.isEmpty(this.pincode)) {
-      this.currentDate = moment().add(1, 'day').format('DD-MM-YYYY');
-      this.dataService.getVaccineAvailability(this.pincode, this.currentDate)
+      this.today = moment().format('DD-MM-YYYY');
+      this.tommorrow = moment().add(1, 'day').format('DD-MM-YYYY');
+      this.isFetching = true;
+      Promise.all([this.dataService.getVaccineAvailability(this.pincode, this.today),
+      this.dataService.getVaccineAvailability(this.pincode, this.tommorrow)])
         .then((data) => {
           this.updatedOn = moment().valueOf();
           this.availableVaccineHospitalList = [];
-          const centers = data.centers as [];
-          console.log(centers);
-          _.forEach(centers, (center) => {
-            const numberOfVaccineAvailable = this.getAvailableVaccineCount(center);
-            if (numberOfVaccineAvailable > 0) {
-              this.playAudioAndVibrate();
-            }
-            this.availableVaccineHospitalList.push({ name: center.name, numberOfVaccineAvailable });
+          const todayCenter = data[0].centers;
+          const tommorrowCenter = data[1].centers;
+          console.log(todayCenter, tommorrowCenter);
+          _.forEach(todayCenter, (center) => {
+            const numberOfVaccineAvailableToday = this.getAvailableVaccineCount(center);
+            this.availableVaccineHospitalList.push({ name: center.name, numberOfVaccineAvailableToday });
           });
+          _.forEach(tommorrowCenter, (center) => {
+            const numberOfVaccineAvailableTommorow = this.getAvailableVaccineCount(center);
+            const index = this.availableVaccineHospitalList.findIndex(x => x.name === center.name);
+            if (index) {
+              this.availableVaccineHospitalList[index].numberOfVaccineAvailableTommorow = numberOfVaccineAvailableTommorow;
+            } else {
+              this.availableVaccineHospitalList.push({ name: center.name, numberOfVaccineAvailableTommorow });
+            }
+          });
+          const playAudiAndVibrate = _.some(this.availableVaccineHospitalList, (availableObject) => {
+            if (availableObject.numberOfVaccineAvailableToday > 0 || availableObject.numberOfVaccineAvailableTommorow > 0) {
+              return true;
+            }
+          });
+          if (playAudiAndVibrate) {
+            this.playAudioAndVibrate();
+          }
+        }).finally(() => {
+          setTimeout(() => { this.isFetching = false; }, 1000);
         });
     }
   }
@@ -65,4 +87,11 @@ export class HomePage {
     clearInterval(this.interval);
     this.interval = undefined;
   }
+}
+
+
+export interface VaccineAvailability {
+  name: string;
+  numberOfVaccineAvailableToday?: number;
+  numberOfVaccineAvailableTommorow?: number;
 }
